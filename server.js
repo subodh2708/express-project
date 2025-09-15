d66c9951084fe2e6de3c5a30b330765edb6c38e2
Subodh <subodhlst@gmail.com>
2025-09-15 22:26:07 +0530

    post request in express

--- server.js
+++ server.js
@@ -10,6 +10,31 @@ const friends = [
   { id: 2, name: "Avantika singh" },
 ];
 
+app.use((req, res, next) => {
+  const start = Date.now();
+  next();
+  const delta = Date.now() - start;
+  console.log(`${req.method} ${req.url} ${delta}ms`);
+});
+
+app.use(express.json());
+
+app.post("/friends", (req, res) => {
+  if (!req.body.name) {
+    return res.status(400).json({
+      error: "missing friend name",
+    });
+  }
+
+  const newFriend = {
+    name: req.body.name,
+    id: friends.length,
+  };
+
+  friends.push(newFriend);
+  res.json(newFriend);
+});
+
 app.get("/friends", (req, res) => {
   res.send(friends);
 });
